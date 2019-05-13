5f8f5a9bfce92c92bce7f33615ce04e523371e9d
Karol Milewczik <kmphj1996@wp.pl>
2019-05-13 22:34:54 +0200

    Stworzenie i wyswietlanie wyniku punktowego w głównym menu oraz kilka drobnych poprawek graficznych

---
+++ apk_snooker — kopia 13-05-2019/apk_snooker/Gracz.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apk_snooker
+{
+    class Gracz
+    {
+        private String pseudonim;
+        public Gracz(String pseudonim)
+        {
+            this.pseudonim = pseudonim;
+        }
+        public string Pseudonim { get => pseudonim; set => pseudonim = value; }
+    }
+}
